0f1bfcb5dcd48753c135a27b692eb43d772ced26
Matias Chaves <matiaschaves01@gmail.com>
2023-08-05 18:59:56 -0300

    tarjetas index

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,7 +6,9 @@ import { Link } from "react-router-dom";
 const user = {
   name: 'Tom Cook',
   email: 'tom@example.com',
-  imageUrl:''}
+  imageUrl:
+    'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+}
 const navigation = [
   { name: 'Dashboard', href: '#', current: true },
   { name: 'Team', href: '#', current: false },
@@ -209,7 +211,9 @@ export default function Home() {
           <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">{/* Your content */}</div>
         </main>
       </div>
+      
     </>
+    
   )
 }
 
